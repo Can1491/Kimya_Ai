eddafe3c3016421a7a6cebb76ba188f003f067a3
Can <69465512+Can1491@users.noreply.github.com>
2026-04-15 17:13:01 +0300

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('models/gemini-pro')
 else:
     print("HATA: GEMINI_API_KEY bulunamadı! Render panelini kontrol et.")
 
